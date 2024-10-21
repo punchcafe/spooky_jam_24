af024c98981f04947f1de8c62fa101c57d74b7ef
Luca Fruzza <lfruzza@hotmail.co.uk>
2024-10-21 22:43:16 +0100

    fix: fix issues around moving platforms

--- platforms/moving_platform.gd
+++ platforms/moving_platform.gd
@@ -6,32 +6,44 @@ class_name MovingPlatform
 @export var initial_y := 1.0
 # TODO: rename total_angle_change
 @export var angle_change := 10.0
-@export var speed_degrees_per_second := 4.0
+# How many times to complete a length (half full oscillation) in 1s
+@export var half_frequency := 0.5
 @export var vertical_change := 0.0
+# TODO: separate union for vertical ONLY changes and dedicates speed.
+# Maybe frequency is a better parameter?
 @export var show_end_position := false
 
-var _current_angle_change := 0.
+var _current_completion_fraction := 0.
 var _interpolation_weight_change_sign = 1.0
 var _from_transform : Transform3D
 var _to_transform : Transform3D
 
 func _ready() -> void:
+	_from_transform = _starting_transform()
+	print(initial_y)
+	print(angle_change)
+	print(initial_rotation_deg)
+	_to_transform = _destination_transform(_from_transform)
 	super()
+	
+func configure_transforms():
 	_from_transform = _starting_transform()
 	_to_transform = _destination_transform(_from_transform)
-	
-	
+
+func _fraction_increase(delta):
+	# The fraction of total travel which should be done this frame
+	return half_frequency * delta * 2
+
 func _process(delta: float) -> void:
 	if Engine.is_editor_hint():
 		_in_editor_updates()
 		return
-	var new_angle_change = _current_angle_change + (self._interpolation_weight_change_sign * delta * self.speed_degrees_per_second)
-	var completion_ratio = new_angle_change / self.angle_change
-	if(completion_ratio <= 0 or completion_ratio >= 1):
+	var new_completion_fraction = self._current_completion_fraction + (self._interpolation_weight_change_sign * _fraction_increase(delta))
+	if(new_completion_fraction <= 0 or new_completion_fraction >= 1):
 		self._interpolation_weight_change_sign *= -1.0
 	else:
-		self.transform = _from_transform.rotated(Vector3(0, 1, 0), deg_to_rad(self.angle_change) * completion_ratio).translated(Vector3(0., self.vertical_change * completion_ratio, 0.))
-		self._current_angle_change = new_angle_change
+		self.transform = _from_transform.rotated(Vector3(0, 1, 0), deg_to_rad(self.angle_change) * new_completion_fraction).translated(Vector3(0., self.vertical_change * new_completion_fraction, 0.))
+		self._current_completion_fraction = new_completion_fraction
 		
 func _destination_transform(initial_transform: Transform3D) -> Transform3D:
 	return _from_transform.rotated(Vector3(0, 1, 0), deg_to_rad(self.angle_change)).translated(Vector3(0., self.vertical_change, 0.))
--- platforms/moving_platform_proxy.gd
+++ platforms/moving_platform_proxy.gd
@@ -2,20 +2,24 @@
 extends Node3D
 # I hate this, but not sure of an easier way to pass properties down
 
-@export var initial_rotation_deg := 0.0
-@export var initial_y := 1.0
-@export var angle_change := 10.0
-@export var speed_degrees_per_second := 3.0
-@export var vertical_change := 0.0
+@export var initial_rotation_deg : float
+@export var initial_y : float
+@export var angle_change : float
+@export var half_frequency : float
+@export var vertical_change : float
 @export var show_end_position := false
 
 func _ready() -> void:
-	pass
+	_pass_values()
+	$MovingPlatform.configure_transforms()
 
 func _process(delta: float) -> void:
+	_pass_values()
+
+func _pass_values() -> void:
 	$MovingPlatform.initial_rotation_deg = self.initial_rotation_deg
 	$MovingPlatform.initial_y = self.initial_y
 	$MovingPlatform.angle_change = self.angle_change
-	$MovingPlatform.speed_degrees_per_second = self.speed_degrees_per_second
+	$MovingPlatform.half_frequency = self.half_frequency
 	$MovingPlatform.vertical_change = self.vertical_change
 	$MovingPlatform.show_end_position = self.show_end_position
