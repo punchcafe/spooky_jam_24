6faa87988f0919b3f70ae96dc5f8ab0bc3a530d6
Luca Fruzza <lfruzza@hotmail.co.uk>
2024-11-13 18:48:24 +0000

    feat: add fade out zoom to start menu

--- starting_menu.gd
+++ starting_menu.gd
@@ -1,19 +1,26 @@
 extends Control
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$StartButton.grab_focus()
+	get_node("Control/StartButton").grab_focus()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("exit_game"):
 		get_tree().quit()
 
 
 func _on_start_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://root_level.tscn") # Replace with function body.
+	$Control.queue_free()
+	var tween := get_tree().create_tween()
+	var tween_time := 0.3
+	tween.tween_property($BlackOverlay, "color:a", 1, tween_time)
+	var camera := get_node("../OutsideScene/Camera3D")
+	var destination_transform := (get_node("../GameStartCameraPosition") as Node3D).global_transform
+	tween.parallel().tween_property(camera, 
+		"global_transform:origin", 
+		destination_transform.origin, 
+		tween_time
+	)
+	tween.tween_callback(func(): get_tree().change_scene_to_file("res://root_level.tscn") )
 
 
 func _on_credits_pressed() -> void:
