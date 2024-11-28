55e85e7f8cfcf95c91ed5e427e81b7d53acf299e
Luca Fruzza <lfruzza@hotmail.co.uk>
2024-11-28 00:30:54 +0000

    feat: add menu sound effects

--- starting_menu.gd
+++ starting_menu.gd
@@ -1,7 +1,11 @@
 extends Control
 
 func _ready() -> void:
+	get_viewport().connect("gui_focus_changed", self._on_focus_changed)
 	get_node("VBoxContainer/HBoxContainer/StartButton").grab_focus()
+
+func _on_focus_changed(_control):
+	$FocusChangedSound.play()
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("exit_game"):
@@ -11,7 +15,8 @@ func _process(delta: float) -> void:
 func _on_start_button_pressed() -> void:
 	$VBoxContainer.queue_free()
 	var tween := get_tree().create_tween()
-	var tween_time := 0.3
+	var tween_time = $GameStartSound.get_stream().get_length()
+	$GameStartSound.play()
 	var camera := get_node("../OutsideScene/Camera3D")
 	var destination_transform := (get_node("../GameStartCameraPosition") as Node3D).global_transform
 	tween.tween_property($BlackOverlay, "color:a", 1.0, tween_time).set_ease(Tween.EASE_IN)
